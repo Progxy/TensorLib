30f53e25c7d46d7a2467d4c0b5e74a7a875009e6
TheProgxy <theprogxy@gmail.com>
2024-05-14 23:57:15 +0200

    fix

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "./include/autograd.h"
 
+void test();
+
 int main() {
     unsigned int shape[] = {1};
     float val = 1.0f;
@@ -23,5 +25,35 @@ int main() {
     derive_node(x.grad_node);
     printf("expr_val: %f, expr_derivative_val: %f\n", CAST_PTR(h.data, float)[0], CAST_PTR(CAST_PTR(x.grad_node, GradNode) -> derived_value.data, float)[0]);
     DEALLOCATE_GRAD_GRAPHS(x.grad_node, x1.grad_node, x2.grad_node, x3.grad_node, x4.grad_node);
+    test();
     return 0;
+}
+
+void test() {
+    unsigned int shape[] = { 1 };    
+    Tensor a = alloc_tensor(shape, ARR_SIZE(shape), FLOAT_32);
+    Tensor b = alloc_tensor(shape, ARR_SIZE(shape), FLOAT_32);
+    Tensor e = alloc_tensor(shape, ARR_SIZE(shape), FLOAT_32);
+    alloc_grad_graph_node(a.data_type, &a);
+    alloc_grad_graph_node(b.data_type, &b);
+    float val = 1.0f;
+    float val_e = -1.0f;
+    fill_tensor(&val_e, a);
+    fill_tensor(&val, b);
+    fill_tensor(&val_e, e);
+    Tensor c = empty_tensor(a.data_type);
+    TENSOR_GRAPH_EXP(&c, a, a.data_type);
+    Tensor pc = empty_tensor(a.data_type);
+    TENSOR_GRAPH_POW(&pc, c, &val_e, c.data_type);
+    Tensor d = empty_tensor(a.data_type);
+    TENSOR_GRAPH_SUM(&d, b, pc);
+    Tensor f = empty_tensor(a.data_type);
+    TENSOR_GRAPH_POW(&f, d, &val_e, d.data_type);
+    derive_node(a.grad_node);
+    printf("f: %f, df/da: %f\n", CAST_PTR(f.data, float)[0], CAST_PTR(CAST_PTR(a.grad_node, GradNode) -> derived_value.data, float)[0]);
+    deallocate_grad_graph(a.grad_node);
+    float res = 0.0f;
+    sigmoid_func(&val_e, &res, FLOAT_32);
+    printf("f: %f, df/da: %f\n", res, res * (1.0f - res)); 
+    return;
 }
